504e93c5f94ce746bce7d7e7ce9bc60117e9f452
Mai Mee <maimee2000@gmail.com>
2019-03-30 15:07:47 +0700

    Trying out new way to code
    Still going to continue DDD but I'll put optimization to the back of my mind.

--- total_time_spent.pyi
+++ total_time_spent.pyi
@@ -30,6 +30,394 @@ Mar 26, 2019 (Tue)  1 hour start making the XX engine (main program)?
 Mar 28, 2019 (Thu)  2 hours refactoring (and broking something) getting new things ready and
                             optimizing key's animation stack
                     5 min   after showering break + Mo's advice solved the problem
-                    1 hour
-
+Mar 29, 2019 (Fri)  5 hours botching together an update
+                    4 hours rethinking, re-planning, and trying to make documentations before
+                    refactoring. (DDD, I guess.)
+Mar 30, 2019 (Sat)  2 hours DDD (cont.)
 """
+from __future__ import annotations
+
+from pathlib import Path
+from typing import Any, Union, Optional, Tuple, List, Dict, NewType
+
+import arcade
+import pyglet
+
+
+class TimeEngine:
+    """ Manages time """
+    def __init__(self):
+        pass
+
+    def tick(self):
+        """ Call to signify one frame passing """
+        pass
+
+    def fps(self) -> float:
+        """ Return current average fps """
+        pass
+
+    def game_time(self) -> float:
+        """ Return current game_time in seconds """
+        pass
+
+    def dt(self) -> float:
+        """ Return time difference of this and last frame in seconds """
+        pass
+
+
+class AudioEngine:
+    """ Manages audio """
+    def __init__(self):
+        pass
+
+    def play(self):
+        """ Play the current song and change game.state accordingly.
+        Resume counting game_time. """
+        pass
+
+    def pause(self):
+        """ Pause the current song and change game.state accordingly.
+        Pause game_time """
+        pass
+
+    def restart(self):
+        """ Reset and pause the current song and change game.state
+        accordingly. Set game_time to zero - wait_time. """
+        pass
+
+    def load(self, path: Optional[Path]):
+        """ Get things going. (Re)Load file from path and initialize
+        for playing. Set game_time to zero - wait_time. """
+        pass
+
+
+class Beatmap:
+    """ Represents information from .osu + .msc files """
+    def __init__(self, path: Path):
+        """ Load information from file at path and create appropriate
+        fields. """
+        pass
+
+    def audio_path(self) -> Path:
+        """ Return path to audio-- the song (mostly .mp3) --file of the
+        instance. """
+        pass
+
+    def audio(self) -> pyglet.media.Source:
+        """ Return an audio object that can be played, paused, replayed
+        , and set time. """
+        pass
+
+    def hit_sound_paths(self) -> List[Path]:
+        """ Return a list of paths to hit_sounds-- sounds a keypress
+        maps to (mostly .wav) --files of the instance. """
+        pass
+
+    def hit_sounds(self) -> List[pyglet.media.Source]:
+        """ Return a list of objects that can be played, paused,
+        replayed, and set time. """
+        pass
+
+    def background_path(self) -> Path:
+        """ Return path to background image(s?) of the instance """
+        pass
+
+    def background_image(self) -> pyglet.image.AbstractImage:
+        """ Return an image that can be drawn """
+        pass
+
+    def video_path(self) -> Path:
+        """ Return path to video of the instance """
+        pass
+
+    def video(self) -> pyglet.media.Source:
+        """ Return a video that can be played, played, paused, replayed
+        , and set time. """
+        pass
+
+    def metadata(self) -> Union[List, Tuple]:
+        """ Return the metadata of the instance """
+        pass
+
+    def version(self) -> str:
+        """ Return the version-- difficulty --of the instance """
+        pass
+
+    def BPM(self) -> float:
+        """ Return the BPM-- beats per minute --of the instance """
+        pass
+
+    def AR(self) -> float:
+        """ Return the AR-- approach rate --of the instance """
+        pass
+
+    def hit_objects(self) -> List[HitObject]:
+        """ Return a list of processed hit_objects of the instance """
+        pass
+
+
+class HitObject:
+    """ Represents an Osu! HitObject """
+    def __init__(self):
+        pass
+
+    def reach_time(self) -> float:
+        """ Return the time (seconds) in relation to game_time the
+        instance need to be hit. """
+        pass
+
+    def reach_time_ms(self) -> int:
+        """ Return the time (milliseconds) in relation to game_time the
+        instance need to be hit. """
+        pass
+
+    def start_time(self) -> float:
+        """ Return the time (seconds) in relation to game_time the
+        instance need to start at. """
+        pass
+
+    def start_time_ms(self) -> int:
+        """ Return the time (milliseconds) in relation to game_time the
+        instance need to start at """
+        pass
+
+    def symbol(self) -> int:
+        """ Return the symbol of the key the instance need to come in """
+        pass
+
+    def sound(self) -> pyglet.media.Source:
+        """ Return a sound object that can be played """
+        pass
+
+    def state(self):
+        """ Return the current state of the instance """
+        pass
+
+    def grade(self) -> Optional[str]:
+        """ Return the grade-- #TODO FIND A RUBRIC --of the instance if
+        gradable. None otherwise. """
+        pass
+
+    def press_time(self) -> float:
+        """ Return the time (seconds) in relation to game_time the
+        instance was pressed. """
+        pass
+
+    def dt(self) -> float:
+        """ Return the time difference (seconds) between reach_time and
+        press_time-- press_time - reach_time --of the instance.
+        Negative if pressed early; positive if late. """
+        pass
+
+    def accuracy(self) -> Optional[float]:
+        """ Return the accuracy-- #TODO FIND A RUBRIC --of the instance
+        if graded. None otherwise. """
+        pass
+
+    def combo(self) -> int:
+        """ Return combo at the time the instance was hit. Include the
+        instance in the calculation as well. """
+        pass
+
+
+class Key:
+    """ Represents a key on the virtual keyboard """
+    def __init__(self):
+        pass
+
+    def update(self):
+        """ Advance the instance by one frame """
+        pass
+
+    def draw(self):
+        """ Draw current frame """
+        pass
+
+    def recreate_buffer(self):
+        """ Create VBO object used to draw """
+        pass
+
+    def on_update(self):
+        """ Do things that needs to be done and advance frame as
+        appropriate. """
+        pass
+
+    def on_key_press(self, symbol: int, modifiers: int):
+        """ Handle key pressing event """
+        pass
+
+    def on_key_release(self, symbol: int, modifiers: int):
+        """ Handle key releasing event """
+        pass
+
+    def setup_animation_stack(self, style, color):
+        """ Stores a VBO generator used for the animation """
+        pass
+
+    def state(self):
+        """ Return the current state of the instance """
+        pass
+
+
+class Keyboard:
+    """ Represents the board behind keys """
+    def __init__(self):
+        pass
+
+    def update(self):
+        """ Update the element """
+        pass
+
+    def draw(self):
+        """ Draw the element """
+        pass
+
+    def recreate_buffer(self):
+        """ Create VBO object used to draw """
+        pass
+
+    def location(self) -> (int, int) :
+        """ Return the current location in pixels """
+        pass
+
+    def set_location(self, center_x: int, center_y: int):
+        """ Move the keyboard (and its keys) to new location """
+        pass
+
+
+class ClockUI:
+    """ Represents on-screen clock UI showing game progress """
+    def __init__(self):
+        pass
+
+    def update(self):
+        """ Update the element """
+        pass
+
+    def draw(self):
+        """ Draw the element """
+        pass
+
+
+class ScoreUI:
+    """ Represents on-screen score UI """
+    def __init__(self):
+        pass
+
+    def update(self):
+        """ Update the element's UI to match data """
+        pass
+
+    def draw(self):
+        """ Draw the element """
+        pass
+
+    def score(self):
+        """ Return current score """
+        pass
+
+    def __iadd__(self, other: int):
+        """ Add the current score by `other` """
+        pass
+
+
+class ComboUI:
+    """ Represents on-screen combo UI """
+    def __init__(self):
+        pass
+
+    def update(self):
+        """ Update the element's UI to match data """
+        pass
+
+    def draw(self):
+        """ Draw the element """
+        pass
+
+    def combo(self):
+        """ Return current combo """
+        pass
+
+    def break_(self):
+        """ Break the current combo """
+        pass
+
+    def __iadd__(self, other: int):
+        """ Add to the current combo by `other` """
+        pass
+
+
+class AccuracyBar:
+    """ Represents on-screen accuracy bar UI """
+    def __init__(self):
+        pass
+
+    def update(self):
+        """ Update the element's UI to match data """
+        pass
+
+    def draw(self):
+        """ Draw the element """
+        pass
+
+    def average_accuracy(self):
+        """ Return current average accuracy """
+        pass
+
+    def stack_len(self):
+        """ Return the number of last hit_objects used to calculate
+        average_accuracy and shown on-screen. """
+        pass
+
+    def set_stack_len(self, new_len: int):
+        """ Change stack_len to new_len """
+        pass
+
+
+class GameWindow(arcade.Window):
+    def __init__(self):
+        """ Create game window """
+        super().__init__(1920, 1080, "musicality",
+                         fullscreen=True, resizable=False, update_rate=1/60)
+
+    def update(self, delta_time: float):
+        pass
+
+    def on_update(self, delta_time: float):
+        pass
+
+    def on_draw(self):
+        pass
+
+    def on_key_press(self, symbol: int, modifiers: int):
+        pass
+
+    def on_key_release(self, symbol: int, modifiers: int):
+        pass
+
+    def on_mouse_press(self, x: float, y: float, button: int, modifiers: int):
+        pass
+
+    def on_mouse_drag(self, x: float, y: float, dx: float, dy: float,
+                      buttons: int, modifiers: int):
+        pass
+
+    def on_mouse_release(self, x: float, y: float, button: int, modifiers: int):
+        pass
+
+    def on_mouse_scroll(self, x: int, y: int, scroll_x: int, scroll_y: int):
+        pass
+
+    def update_rate(self):
+        """ Return the current ideal update rate """
+        pass
+
+    def set_update_rate(self, rate: float):
+        """ Set the update rate. Cascade throughout the objects. """
+        pass
+
+    def state(self):
+        """ Return current state of the instance """
+        pass
+
